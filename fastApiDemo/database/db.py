7d52c1fc79c8f5dafb92639da23f203242a6f725
david <davidray053@gmail.com>
2026-02-17 00:06:16 +0530

    fastAPi project completed

--- fastApiDemo/database/db.py
+++ fastApiDemo/database/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
+# this file is for pydantic
 db_url = "postgresql://postgres:root@localhost:5432/fastapi"   # postgres:root  <- user name and password
 engine = create_engine(db_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- fastApiDemo/main.py
+++ fastApiDemo/main.py
@@ -1,10 +1,50 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from models.models import Product
 
-from database.db import session
+from database.db import session, engine
+from database import database_models
+
+from sqlalchemy.orm import Session
 
 app = FastAPI()
+
+# for CORS permission
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+)
+
+
+# below line for creating table in database through alchemy
+database_models.Base.metadata.create_all(bind=engine)
+
+# function for initilizating with data if table is empty in DB else dont do anything
+def init_db():
+    db = session()
+
+    count = db.query(database_models.Product).count()  # for reloading server without trying to save data again
+    if count == 0:
+        for product in products:
+            db.add(database_models.Product(**product.model_dump()))
+
+    db.commit()
+
+init_db()
+# db = session() is used multiple times, which is bad, as we're not also closeding it.
+# so create a function to have one connection
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
 @app.get("/")
 def greet():
     return "welcome to first app"
@@ -25,29 +65,63 @@ products = [
     Product(id=4, name="Table", description="A wooden table", price=199.99, quantity=20),
 ]
 
+
+
+
 @app.get("/products")
-def get_all_products():
+def get_all_products(db: Session = Depends(get_db)):    # (db: Session = Depends(get_db) -> when use single db session to inject in the method
     # create db session
-    db = session()
+    #db = session()
     # perform query
-    db.query(Product).all()
-    return products
+    #db.query(Product).all()
+    #return products
+
+    # after db is injected
+    db_products = db.query(database_models.Product).all()
+
+    return db_products
 
 @app.get("/products/{id}")
-def get_product_by_id(id:int):
+def get_product_by_id(id:int, db: Session = Depends(get_db)):   # from DB
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if db_product:
+        return db_product
+
+    return "product not found"
+'''
+def get_product_by_id(id:int):  # for list 
    # return products[id-1]
    for p in products:
        if p.id == id:
            return p
 
    return "product not found"
-
+'''
 @app.post("/products")
+def add_product(product: Product, db: Session = Depends(get_db)):   # for db
+    db.add(database_models.Product(**product.model_dump()))
+    db.commit()
+    return product
+'''
+# for locla list
 def add_product(product: Product):
     products.append(product)
     return product
-
+'''
 @app.put("/products/{id}")
+def update_product(id: int, product: Product, db: Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if db_product:
+        db_product.name = product.name
+        db_product.description = product.description
+        db_product.price = product.price
+        db_product.quantity = product.quantity
+        db.commit()
+        return "product updated successfully"
+    else:
+        return "No product found"
+'''
+# for local list
 def update_product(id: int, product: Product):
     for i in range(len(products)):
         if products[i].id == id:
@@ -55,8 +129,17 @@ def update_product(id: int, product: Product):
             return "product updated successfully"
 
     return "product not found"
-
+'''
 @app.delete("/products/{id}")
+def delete_product(id: int, db: Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if db_product:
+        db.delete(db_product)
+        db.commit()
+    else:
+        return "product not found"
+
+'''
 def delete_product(id: int):
     for i in range(len(products)):
         if products[i].id == id:
@@ -64,3 +147,9 @@ def delete_product(id: int):
             return "product deleted successfully"
 
     return "product not found"
+'''
+
+
+
+
+
